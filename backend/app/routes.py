daa21558dec084874431dc3c1ad4882443607688
deekshithr999 <reddydeekshith999@gmail.com>
2025-04-05 23:14:09 -0400

    commit #2 Included password hashing

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, request, jsonify
 from .models import User
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return {"message": "Hello from Flask backend!"}
+
 
 @main.route('/register', methods=['POST'])
 def register():
@@ -20,8 +24,23 @@ def register():
     if User.query.filter_by(username=username).first():
         return jsonify({"error": "Username already exists"}), 409
 
-    user = User(username=username, password=password)
+    hashed_pw = generate_password_hash(password)
+    user = User(username=username, password=hashed_pw)
+
     db.session.add(user)
     db.session.commit()
 
     return jsonify({"message": "User registered successfully"}), 201
+
+
+@main.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({"error": "Invalid username or password"}), 401
+
+    return jsonify({"message": "Login successful"}), 200
